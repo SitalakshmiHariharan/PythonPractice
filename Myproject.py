4ecb71b8452ad11f191464127219b37abd176f85
Sita <30361057+SitalakshmiHariharan@users.noreply.github.com>
2021-05-10 18:26:35 -0400

    Add files via upload

---
+++ Myproject.py
@@ -0,0 +1,13 @@
+import sys
+n = sys.argv[0]
+a = sys.argv[1]
+b = sys.argv[2]
+print(n)
+c = int(a) + int(b)
+d = eval(input('Enter an exp'))
+print(d)
+print(c)
+x = int(input("Enter 1st number :"))
+y = int(input('Enter 2nd number :'))
+z = x + y
+print(z)
---
+++ PythonPractice.py
@@ -0,0 +1,20 @@
+
+#x = input("Enter the first number:")
+#a=int(x[0])
+#a=int(input("Enter your first number: "))
+#b =int(input("Enter your second number: "))
+#result = eval('a/b')
+#print (result)
+#import sys
+#i = sys.argv[0]
+#j = int(sys.argv[1])
+#k = int(sys.argv[2])
+#print(i,j + k)
+
+import math
+    # Write your code here
+
+seta = {1,2,3,4}
+setb = {5,6,7,8}
+setc = seta.union(setb)
+print(setc)
